b2847bc0017c1c2cafeb78b1fc49aeccda5cbccb
ivanva2 <vasiliev.ivan2003@yandex.ru>
2023-04-11 20:14:23 +0300

    z

--- my_site/src/App.js
+++ my_site/src/App.js
@@ -1,20 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 import './mystyle.css';
 import React, { useEffect, useState } from 'react';
-import { Card, Row } from 'antd';
-import lg from './img/logo192.png'
+import { Card, Row } from 'antd'
 
-class Header extends React.Component
-{
-  render ()
-  {
 
-    return <header>Чья-то шапка. Пожалуйста, не судите строго, я не умею в дизайн.</header>
 
-  }
+function changeBackground(color) {
+  document.body.style.background = color;
 }
 
+window.addEventListener("load",function() { changeBackground('#9d8ef6') });
 function App() {
   const [users, setUsers] = useState([]);
   const [userPosts, setPosts] = useState([]);
@@ -37,9 +33,7 @@ function App() {
     })
   }
 
-  const loadUsers = () => {
-    getData();
-  }
+  
 
   useEffect(()=>{
     getData();
@@ -48,21 +42,22 @@ function App() {
 
   return(
     <>
-      <Header />
-      <h2><img src={lg} style = {{width: '50px', height: '50px'}}></img>{" Учу React :)"}</h2>
+      
+      
       <div style={{margin: '0 auto', display: 'grid', gap: 10, width: 850 }}>
         {users.length > 0 && users.map(user => {
             return (
-            <Card class='AuthorLib' title={`Автор: ${user.name}`} key={Math.random()} style = {{borderColor: '#aaaaaa', backgroundColor: '#aaaaaa'}}>
+            <Card class='AuthorLib' title={`Author: ${user.name}`} key={Math.random()} style = {{borderColor: '#aed2ea', backgroundColor: '#aed2ea'}}>
               
               <p>Email adress: <strong style = {{color: 'red'}}>{user.email}</strong></p>
-
+              <p>Phone nomber: <strong style = {{color: 'yellow'}}>{user.phone}</strong></p>
+              
               <Row gutter={10}>  
                {
                 userPosts.filter(post => post.userId === user.id).map(post =>{
                   return(
 
-                      <Card title={post.title} style={{margin:10, borderColor: '#aaaaaa'}} headStyle={{backgroundColor: '#282c34', color: '#fff'}}>
+                      <Card title={post.title} style={{margin:10, borderColor: '#aed2ea'}} headStyle={{backgroundColor: '#3352ff', color: '#fff'}}>
                         
                         <p class = 'Paragraph'>{post.body}</p>
 
